38e37fc51896027bf07105dc6c5d53d4ef2569f9
unknown <medin_93@live.com>
2019-05-02 22:41:48 +0200

    Methoden, um Rover korrekt zu bewegen

--- src/main/java/rover/Rover.java
+++ src/main/java/rover/Rover.java
@@ -1,10 +1,6 @@
 package rover;
 
-import java.util.LinkedHashMap;
-
 import java.util.Random;
-import java.util.Set;
-import java.util.Map.Entry;
 
 public class Rover {
 
@@ -71,6 +67,95 @@ public class Rover {
 
             default:
                 throw new IllegalArgumentException("roverRichtung");
+        }
+    }
+    /**
+     * Bewege ein Objekt, das sich an der angegebenen Position befindet. Muss ein Rover am Standort sein.
+     *
+     * Ueberprueft, dass die Karte nicht verlassen wird und sich nicht in leeren Feldern bewegt.
+     * @param position Position zum Ueberpruefen
+     * @param nachVorne Sollte sich der Rover vorwaerts (wahr) oder rueckwaerts (falsch) bewegen
+     * @return Neue Position oder alte Position, wenn neue Position ungueltig war (blockiert, auﬂerhalb der Karte)
+     */
+    public static Position bewegeRover (Position position, boolean nachVorne) {
+        // Bekommt neue Position wenn sich bewegt
+        Position neuePosition = getNeuePosition(position, nachVorne);
+
+        // Pruefe die neue Position
+        if (!neuePosition.pruefeMaxHoeheUndBreite(KARTE_BREITE, KARTE_HOEHE)) return position;
+
+        // Pruefen, ob die neue Position blockiert ist
+        if (MARS_KARTE.isBlockiert(neuePosition)) return position;
+
+        // Bewege den Rover
+        MARS_KARTE.bewegeUndFuegeNeuePositionEin(position, neuePosition);
+
+        // return die neue Position
+        return neuePosition;
+    }
+
+    /**
+     * Bekommt neue Position wenn sich in eine Richtung bewege.
+     * @param position Aktuelle position
+     * @param forward Vorwaerts oder rueckwaerts bewegen
+     * @return neue Position.
+     */
+    private static Position getNeuePosition(Position position, boolean nachVorne) {
+        Feldinhalt roverRichtung = MARS_KARTE.getPositionMitFeldinhalt(position);
+
+        switch (roverRichtung) {
+            case ROVER_NORD:
+                return new Position(
+                        position.getX(),
+                        nachVorne ? position.getY() - 1 : position.getY() + 1);
+
+            case ROVER_OST:
+                return new Position(
+                        nachVorne ? position.getX() + 1 : position.getX() - 1,
+                        position.getY());
+
+            case ROVER_SUED:
+                return new Position(
+                        position.getX(),
+                        nachVorne ? position.getY() + 1 : position.getY() - 1);
+
+            case ROVER_WEST:
+                return new Position(
+                        nachVorne ? position.getX() - 1 : position.getX() + 1,
+                        position.getY());
+
+            default:
+                throw new IllegalArgumentException("roverRichtung");
+        }
+
+    }
+
+    /**
+     * Behandle gegebenen Befehle
+     * @param Befehl zu behandeln.
+     */
+    public static void behandleBefehle(Befehle befehle) {
+        Feldinhalt roverRichtung = MARS_KARTE.getPositionMitFeldinhalt(ROVER_POSITION);
+        switch (befehle) {
+            case NACH_VORNE:
+                ROVER_POSITION = bewegeRover(ROVER_POSITION, true);
+                break;
+
+            case RUECKWAERTS:
+                ROVER_POSITION = bewegeRover(ROVER_POSITION, false);
+                break;
+
+            case LINKS_ABBIEGEN:
+                roverRichtung = linksAbbiegen(roverRichtung);
+                MARS_KARTE.setzePositionUndFeldinhalt(ROVER_POSITION, roverRichtung);
+                break;
+            case RECHTS_ABBIEGEN:
+                roverRichtung = rechtsAbbiegen(roverRichtung);
+                MARS_KARTE.setzePositionUndFeldinhalt(ROVER_POSITION, roverRichtung);
+                break;
+
+            default:
+                System.out.println("Unknown command: " + befehle);
         }
     }
     public static void printKarte () {
--- src/main/java/rover/Test.java
+++ src/main/java/rover/Test.java
@@ -6,14 +6,13 @@ public class Test {
 		if (args.length > 1) {
 			long seed = Long.parseLong(args[1]);
 			Rover.ZUFALL_ZAHL.setSeed(seed);
-			// System.out.println("Seed: " + seed);
 		}		
 		Rover.erstelleKarteUndSetzeRover();
-		String directionsOfRover = args[0];
+		String commands = args[0];
 		Rover.printKarte();
 		
-		for(int i=0; i < directionsOfRover.length(); i++) {
-			//Rover.roverMovement(directionsOfRover.charAt(i));
+		for(char befehle: commands.toCharArray()) {
+			Rover.behandleBefehle(Befehle.vonChar(befehle));
 			Rover.printKarte();
 		}
 	}
